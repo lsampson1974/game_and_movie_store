1e15cd8eaa0e86b4a6ecb0ee1ac24af3ddeac5b5
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-04 18:12:11 +0000

    Adding test cases for program

--- stockroom.rb
+++ stockroom.rb
@@ -1,5 +1,5 @@
 # This module is to be run at the command line.
-require_relative 'stock' 
+require_relative 'inventory' 
 
 stockroom_sim = Stockroom.new
 stockroom_sim.main_menu 
---
+++ test/test_input_checks.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require './inventory'
+
+class TestInputChecks < Minitest::Test
+
+  def test_menu
+    stockroom = Stockroom.new
+  end
+
+  
